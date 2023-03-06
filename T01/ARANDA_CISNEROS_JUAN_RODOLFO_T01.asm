550dae382f11086d3dd13380c793310313b1661a
Juan Rodo <118646317+Juan2442@users.noreply.github.com>
2023-03-05 20:32:47 -0600

    Update ARANDA_CISNEROS_JUAN_RODOLFO_T01.asm

--- T01/ARANDA_CISNEROS_JUAN_RODOLFO_T01.asm
+++ T01/ARANDA_CISNEROS_JUAN_RODOLFO_T01.asm
@@ -1,36 +1,46 @@
 ;Autor: Juan Rodolfo Aranda Cisneros
 ;Fecha: 05/03/2023
 
-org 100h    ; Inicio del programa en la dirección 100h
+.model small
+.stack 100h
 
-.data       ; Definición de los datos
-array dw 6, 2, 8, 4, 5
-n dw 5      ; Tamaño de la lista
+.data
+    array db 6, 3, 9, 1, 8, 2
+    size equ ($ - array)
 
-.code       ; Código del programa
-start:
-    mov cx, n           ; Carga el tamaño de la lista en CX
-    dec cx              ; Disminuye CX en 1 (el índice comienza en 0)
-outer_loop:
-    mov si, 0           ; Carga el índice del primer elemento en SI
-inner_loop:
-    mov ax, [array+si]  ; Carga el elemento actual en AX
-    mov bx, [array+si+2]; Carga el siguiente elemento en BX
-    cmp ax, bx          ; Compara los elementos
-    jle skip_swap       ; Salta si están en orden
-    xchg ax, bx         ; Intercambia los elementos
-    mov [array+si], ax  ; Almacena el elemento intercambiado en el primer índice
-    mov [array+si+2], bx; Almacena el elemento intercambiado en el segundo índice
-skip_swap:
-    add si, 2           ; Avanza al siguiente par de elementos
-    cmp si, cx          ; Compara con CX para verificar si es el último par
-    jl inner_loop       ; Salta si hay más pares por comparar
-    loop outer_loop     ; Repite el proceso hasta que la lista esté ordenada
+.code
+    main proc
+        mov ax, @data
+        mov ds, ax
+        
+        mov cx, size
+        dec cx
 
-    ; Aquí puedes agregar el código para imprimir la lista ordenada
+    outer_loop:
+        mov si, 0
+    inner_loop:
+        mov al, array[si]
+        cmp si, cx
+        jge next_element
+        mov bl, array[si+1]
+        cmp al, bl
+        jle next_element
+        mov array[si], bl
+        mov array[si+1], al
+    next_element:
+        inc si
+        cmp si, cx
+        jle inner_loop
 
-    mov ah, 4ch         ; Carga la función de salida
-    int 21h             ; Salida del programa
+        loop outer_loop
 
-end start   ; Fin del programa
+    ; Imprimir el arreglo ordenado
+        mov ah, 9
+        lea dx, array
+        int 21h
+
+        mov ah, 4ch
+        int 21h
+    main endp
+end main
 
