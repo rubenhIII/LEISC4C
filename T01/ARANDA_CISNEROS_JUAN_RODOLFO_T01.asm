b397af46b123418ecc610be7a75eaf380da2f5de
Juan Rodo <118646317+Juan2442@users.noreply.github.com>
2023-03-05 20:26:18 -0600

    Update ARANDA_CISNEROS_JUAN_RODOLFO_T01.asm

--- T01/ARANDA_CISNEROS_JUAN_RODOLFO_T01.asm
+++ T01/ARANDA_CISNEROS_JUAN_RODOLFO_T01.asm
@@ -1,60 +1,36 @@
 ;Autor: Juan Rodolfo Aranda Cisneros
 ;Fecha: 05/03/2023
 
+org 100h    ; Inicio del programa en la dirección 100h
 
-DATOS SEGMENT
-    ; VARIABLES
-    BURBUJA DB 12, 23, 67, 89, 255
-    ;--------------------------------------------------------------------------
-DATOS ENDS
+.data       ; Definición de los datos
+array dw 6, 2, 8, 4, 5
+n dw 5      ; Tamaño de la lista
 
-PILA SEGMENT
-    DB 64 DUP(0)
-PILA ENDS
+.code       ; Código del programa
+start:
+    mov cx, n           ; Carga el tamaño de la lista en CX
+    dec cx              ; Disminuye CX en 1 (el índice comienza en 0)
+outer_loop:
+    mov si, 0           ; Carga el índice del primer elemento en SI
+inner_loop:
+    mov ax, [array+si]  ; Carga el elemento actual en AX
+    mov bx, [array+si+2]; Carga el siguiente elemento en BX
+    cmp ax, bx          ; Compara los elementos
+    jle skip_swap       ; Salta si están en orden
+    xchg ax, bx         ; Intercambia los elementos
+    mov [array+si], ax  ; Almacena el elemento intercambiado en el primer índice
+    mov [array+si+2], bx; Almacena el elemento intercambiado en el segundo índice
+skip_swap:
+    add si, 2           ; Avanza al siguiente par de elementos
+    cmp si, cx          ; Compara con CX para verificar si es el último par
+    jl inner_loop       ; Salta si hay más pares por comparar
+    loop outer_loop     ; Repite el proceso hasta que la lista esté ordenada
 
-CODIGO SEGMENT
+    ; Aquí puedes agregar el código para imprimir la lista ordenada
 
-    INICIO PROC FAR ;NEAR Y FAR
-    ASSUME DS:DATOS, CS:CODIGO, SS:PILA
-    PUSH DS
-    MOV AX, 0
-    PUSH AX
-    
-    MOV AX, DATOS
-    MOV DS, AX
-    MOV ES, AX
-    
-    ; CODIGO---------------------------------------------
-    MOV CX, 7
-    MOV SI, 0
-    MOV DI, 0
-    
-    CICLO1:
-    PUSH CX ;PONER EN LA PILA EL VALOR DE CX
-    LEA SI, ARRAY_BURBUJA ;PASAR LA DIRECCION EFECTIVA DEL ARREGLO A SI
-    MOV DI, SI  ;Y LUEGO PASARLA A DI  
-           
-    CICLO2:      
-    INC DI    ;INCREMENTAR DI PARA PODER COMPARAR LA SIGUIENTE POSICION
-    MOV AL, [SI] ;PASAR EL VALOR QUE SE ENCUENTRA EN LA DIRECCION DE SI A AL
-    CMP AL, [DI] ; COMPARAR CON EL VALOR QUE SE ENCUENTRA EN LA POSICION DE DI
-    JA INTERCAMBIO ; SALTA A LA ETIQUETA SI ES MAYOR
-    JB MENOR ; SALTA A LA ETIQUETA SI ES MENOR
-    
-    INTERCAMBIO: 
-    MOV AH, [DI] ; MUEVE EL VALOR QUE SE ENCUENTRA EN DI A AH
-    MOV [DI], AL ; MUEVE EL VALOR DE AL A LA POSICION DE DI
-    MOV [SI], AH ; PASA EL VALOR DE AH A LA POSICION DE SI
-    
-    MENOR:  
-    INC SI
-    LOOP CICLO2 
-    POP CX
-    LOOP CICLO1
-    
-    ;---------------------------------------------------------------
-    EXIT:
-    RET
-    INICIO ENDP
-CODIGO ENDS 
-END INICIO
+    mov ah, 4ch         ; Carga la función de salida
+    int 21h             ; Salida del programa
+
+end start   ; Fin del programa
+
