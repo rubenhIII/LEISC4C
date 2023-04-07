3c178c0a64bb9bb0d42a4c8856d9da09c1a19a85
Hector Andres <handres_rock@hotmail.com>
2023-04-06 23:04:02 -0600

    Cambio de precisión y mejoras

--- T04/asm/GUTIERREZ_ESPARZA_HECTOR_ANDRES_T04.asm
+++ T04/asm/GUTIERREZ_ESPARZA_HECTOR_ANDRES_T04.asm
@@ -1,161 +1,111 @@
 section .rodata
-    ; pi: dq 3.1415926535897932
-    maxiter: dq 7.0
-    minusone: dq -1.0
+    maxiter: dd 5
 
 section .data
-    input: dq 4.0
-    base: dq 0.0
-    output: dq 0.0
-    senx: dq 0.0
-    n: dq 0.0
+    input: dd 4.0
+    ioutput: dd 0.0
+    senx: dd 0.0
+    n: dd 0
 
-    ebase: dq 0.0
-    exp: dq 0.0
-    eoutput: dq 1.0
+    sign: dd -1
 
-    foutput: dq 1.0
-    fbase: dq 0.0
-    findex: dq 0.0
+    ebase: dd 0.0
+    exp: dd 0
+    poutput: dd 1.0
+
+    foutput: dd 1
+    fbase: dd 0
 
 section .text
     global main
     main:
-        finit
-        mov eax, 23
-        fldpi
-        ; fld qword [pi]
-        fld qword [input]
-        fdiv
-        fstp qword [input]
-        call calcSin
+        mov dword [n], 0
+        mov eax, 0
+        call calcinput
+        call calcsin
     exit:
         mov eax, 01h
         mov ebx, 00h
         int 80h
 
-    calcSin:
-        ;(-1)^n
-        fld qword [minusone]
-        fstp qword [ebase]
-        fld qword [n]
-        fstp qword [exp]
-        call startExp
-        ;(2n+1)!
-        fld qword [n]
-        fld1
-        fld1
-        fadd
-        fmul
-        fld1
-        fadd
-        fst qword [base]
-        fstp qword [fbase]
-        ;factorial
-        call fact
-        ;division
-        fld qword [eoutput]
-        fld qword [foutput]
+    calcinput:
+        finit
+        fldpi
+        fld dword [input]
         fdiv
-        fstp qword [output]
+        fstp dword [input]
+        ret
+
+    changesign:
+        cmp dword [sign], 1
+        je negate
+        mov dword [sign], 1
+        ret
+    negate:
+        mov dword [sign], -1
+        ret
+
+    calcexp:
+        mov ax, [n]
+        mov bl, 2
+        mul bl
+        add ax, 1
+        mov [exp], ax
+        ret
+
+    calcsin:
+        ;(-1)^n
+        call changesign
+        ;(2n+1)
+        call calcexp
+        ;factorial
+        mov [fbase], ax
+        call calcfact
+        ;[]/[]
+        fild dword [sign]
+        fild dword [foutput]
+        fdiv
+        fstp dword [ioutput]
         ;x^(2n+1)
-        fld qword [input]
-        fstp qword [ebase]
-        fld qword [base]
-        fstp qword [exp]
-        call startExp
-        ;multiplicacion
-        fld qword [output]
-        fld qword [eoutput]
+        fld dword [input]
+        fstp dword [ebase]
+        call calcpow
+        ;[]*[]
+        fld dword [ioutput]
+        fld dword [poutput]
         fmul
-        fst qword [output]
-        ;sumatoria
-        fld qword [senx]
+        fst dword [ioutput]
+        ;x - x^3/3!+...
+        fld dword [senx]
         fadd
-        fstp qword [senx]
-        fld qword [n]
-        fld qword [maxiter]
-        fcompp
-        fstsw ax
-        fld qword [n]
-        fld1
-        fadd
-        fstp qword [n]
-        and eax, 0100011100000000b
-        cmp eax, 0000000000000000b
-        jne bk2
-        call calcSin
-    bk2:
+        fstp dword [senx]
+        inc dword [n]
+        mov eax, [maxiter]
+        cmp eax, [n]
+        jae calcsin
         ret
 
-    startExp:
+    calcpow:
         fld1
-        fstp qword [eoutput]
-        fldz
-        fld qword [exp]
-        fcompp
-        fstsw ax
-
-
-        and eax, 0100011100000000b
-        cmp eax, 0000000000000000b
-        je notazero
-        fld1
-        fstp qword [eoutput]
+        fstp dword [poutput]
+        mov ecx, [exp]
+        cmp dword [exp], 0
+        jne powCalc
         ret
-    notazero:
-        fld qword [eoutput]
-        fld qword [ebase]
+    powCalc:
+        fld dword [poutput]
+        fld dword [ebase]
         fmul
-        fstp qword [eoutput]
-        fldz
-        fld qword [exp]
-        fld1
-        fsub
-        fst qword [exp]
-        fcompp
-        fstsw ax
-        and eax, 0100011100000000b
-        cmp eax, 0100000000000000b
-        je bk
-        call notazero
-    bk:
+        fstp dword [poutput]
+        loop powCalc
         ret
 
-    fact:
-        ; mov ecx, [fbase]
-        
-        fld qword [fbase]
-        fld1
-        fld1
-        fadd
-        fcompp
-        fstsw ax
-        and eax, 0100011100000000b
-        cmp eax, 0000000000000000b
-        fld1
-        fstp qword [foutput]
-        jne canProceed
-        ret
-    canProceed:
-        fld qword [fbase]
-        fstp qword [findex]
+    calcfact:
+        mov dword [foutput], 1
+        mov ecx, [fbase]
     factCalc:
-        fld qword [foutput]
-        fld qword [findex]
-        fmul
-        fstp qword [foutput]
-        
-        fld1
-        fld qword [findex]
-        fld1
-        fsub
-        fst qword [findex]
-        fcompp
-        fstsw ax
-        and eax, 0100011100000000b
-        cmp eax, 0100000000000000b
-        je bk1
-        call factCalc
-    bk1:
+        mov eax, [foutput]
+        mul ecx
+        mov [foutput], eax
+        loop factCalc
         ret
