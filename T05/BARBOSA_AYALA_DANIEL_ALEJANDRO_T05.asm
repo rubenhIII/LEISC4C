d7f08aa51e806b09a1f744ce3609ad214f78ebd4
DanyBarbosa <124807841+DanyBarbosa@users.noreply.github.com>
2023-05-24 11:30:31 -0600

    Add files via upload

---
+++ T05/BARBOSA_AYALA_DANIEL_ALEJANDRO_T05.asm
@@ -0,0 +1,91 @@
+GLOBAL main
+
+section .data
+    archEntrada db "data.txt", 0    ; Nombre del archivo de entrada
+    archSalida db "DABA.txt", 0     ; Nombre del archivo de salida
+    letraMayus db 65                ; Valor ASCII de la letra 'A'
+    letraMinus db 97                ; Valor ASCII de la letra 'a'
+    contNum db 0                    ; Contador de números
+    contLetras db 0                 ; Contador de letras
+    ascii db 0                      ; Variable para almacenar el número convertido a ASCII
+    espacio db " "                  ; Carácter espacio
+    newline db 10                   ; Carácter de nueva línea
+    comp db 1                       ; Variable de comparación
+
+section .bss
+    fd resw 1                       ; Descriptor de archivo
+    fdSalida resw 1                 ; Descriptor de archivo para el archivo de salida
+    buffer resb 1                   ; Búfer para lectura de archivos
+
+section .text
+
+main:
+    ; Abrir el archivo de entrada
+    mov eax, 5
+    mov ebx, archEntrada
+    mov ecx, 2                   ; Bandera para lectura y escritura
+    mov edx, 777                 ; Permisos lectura, escritura y ejecución para todos
+    int 0x80
+
+    mov [fd], eax                 ; Guardar el descriptor de archivo
+
+    read_loop:
+        mov eax, 3
+        mov ebx, dword [fd]
+        mov ecx, buffer
+        mov edx, 1                   ; Longitud del búfer (1 byte)
+        int 0x80
+
+        cmp eax, 0                   ; Comprobar si se llegó al final del archivo
+        jz end                        ; Si es así, salir del bucle
+
+        ; Revisar si es el carácter que queremos
+
+        mov al, [buffer]
+        cmp al, [letraMayus]             ; Comparar para verificar si contiene la letra mayúscula
+        je next
+        cmp al, [letraMinus]             ; Comparar si es la letra en minúscula
+        je next
+
+        jmp read_loop
+
+    end:
+        ; Cerrar el archivo de lectura
+        mov eax, 6
+        mov ebx, [fd]
+        int 0x80
+
+        ; Revisar si se pasó por todas las letras de la A a la Z
+        inc byte [contLetras]
+        cmp byte [contLetras], 27
+        je exit                         ; Salir
+        jl compare                      ; Escribir en archivo
+
+    next:
+        inc byte [contNum]
+        jmp read_loop
+
+escribir:
+    ; Abrir el archivo de escritura
+    mov eax, 5
+    mov
+
+compare:
+    cmp byte [comp], 1
+    je crear
+    jmp escribir
+
+crear:
+    inc byte [comp]
+    mov eax, 8                       ; Número de llamada del sistema para crear un archivo
+    mov ebx, archSalida               ; Nombre del archivo
+    mov ecx, 0777                     ; Permisos (rwx rwx rwx)
+    int 0x80                          ; Realizar la llamada al sistema
+    mov [fdSalida], eax
+
+    ; Cerrar el archivo de escritura
+    mov eax, 6
+    mov ebx, [fdSalida]
+    int 0x80
+
+    jmp escribir
